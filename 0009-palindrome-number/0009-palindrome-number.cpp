d211354044cf55e1731efb09360529d3d0e80e5b
krish4245 <124053970+krish4245@users.noreply.github.com>
2026-07-15 22:20:14 +0530

    Time: 5 ms (19.77%), Space: 8.5 MB (65.85%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int ans = 0;
+        
+        if(x<0){
+            return false;
+        }
+
+       
+        int orignal  = x;
+        while(x!=0){
+            int digit = x%10;
+            if(ans> INT_MAX/10 || ans< INT_MIN/10){
+            return 0;
+            }
+            ans = ans*10 + digit;
+            x= x/10;
+            
+        }
+        return orignal == ans;
+
+    }
+};
